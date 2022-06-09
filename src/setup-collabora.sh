076231085e01b00f649cfa03bb7ebefef22c54f5
Daniel Teichmann <daniel.teichmann@das-netzwerkteam.de>
2022-06-09 12:43:42 +0200

    src/setup-collabora.sh: Adding hostname to /etc/hosts entry.

--- src/setup-collabora.sh
+++ src/setup-collabora.sh
@@ -124,9 +124,10 @@ function step5() {
 
     deploy_file "$TMP_DIR_PATH"/coolwsd.xml /etc/coolwsd/coolwsd.xml || true
 
-    #log "Deleting every '127.0.1.1' entry in /etc/hosts."
-    #sed -i "|127.0.1.1|d"
-    entry="127.0.1.1 $SERVER_FQDN"
+    log "Deleting every '127.0.1.1' entry in /etc/hosts."
+    is_dry_run || sed -i "/127.0.1.1/d" /etc/hosts
+
+    entry="127.0.1.1 $SERVER_FQDN $(hostname)"
     log "Deploying '$entry' in /etc/hosts."
     is_dry_run || echo "$entry" >>/etc/hosts
 
