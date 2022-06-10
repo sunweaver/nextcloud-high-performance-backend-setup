461d66442fd7885244a1585934b8d576a434a54a
Daniel Teichmann <daniel.teichmann@das-netzwerkteam.de>
2022-06-10 13:40:16 +0200

    src/setup-collabora.sh: Warn about potential HTTPS issue in info text.

--- src/setup-collabora.sh
+++ src/setup-collabora.sh
@@ -110,5 +110,7 @@ function collabora_print_info() {
     log "The Collabora Online service got installed. To set it up," \
         "\nlog into your Nextcloud instance with an adminstrator account" \
         "\nand navigate to Settings -> Administration -> Nextcloud Office." \
-        "\nNow select 'Use your own server' and type in '$collabora_address'.\n"
+        "\nNow select 'Use your own server' and type in '$collabora_address'." \
+        "\nPlease note that you need to have a working HTTPS setup on your" \
+        "\nNextcloud server in order to get Nextcloud Office working.\n"
 }
