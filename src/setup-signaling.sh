f16284612ec6200bdfb0695307b3e540e0aa8f31
PhilProg <91820316+PhilDevProg@users.noreply.github.com>
2022-08-31 17:53:13 +0200

    setup-signaling: Rearrange apt commands

--- src/setup-signaling.sh
+++ src/setup-signaling.sh
@@ -43,11 +43,11 @@ EOL
 			if [ "$UNATTENTED_INSTALL" == true ]; then
 				log "Trying unattended install for Signaling."
 				export DEBIAN_FRONTEND=noninteractive
-				apt-get install -qqy protobuf-compiler build-essential make 2>&1 | tee -a $LOGFILE_PATH
 				apt-get install -qqy -t bullseye-backports golang-go 2>&1 | tee -a $LOGFILE_PATH
+				apt-get install -qqy protobuf-compiler build-essential make 2>&1 | tee -a $LOGFILE_PATH
 			else
-				apt-get install -y rotobuf-compiler build-essential make 2>&1 | tee -a $LOGFILE_PATH
 				apt-get install -y -t bullseye-backports golang-go 2>&1 | tee -a $LOGFILE_PATH
+				apt-get install -y rotobuf-compiler build-essential make 2>&1 | tee -a $LOGFILE_PATH
 			fi
 		fi
 
